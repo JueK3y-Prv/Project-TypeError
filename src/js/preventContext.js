b76708645e2b3ede1d888a584608eec2e357e1fd
JueK3y <54510690+JueK3y@users.noreply.github.com>
2021-04-08 14:07:13 +0200

    Updated shopping cart location

--- src/js/preventContext.js
+++ src/js/preventContext.js
@@ -3,9 +3,9 @@
 } */
 
 document.onkeydown = function(e) {
-    if(event.keyCode == 123) {
+    /*if(event.keyCode == 123) {
         return false;
-    }
+    }*/
     if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
         return false;
     }
