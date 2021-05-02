078bd7830a349b0bb08b953f064e27d0c81f93cd
JueK3y <54510690+JueK3y@users.noreply.github.com>
2021-05-02 11:58:54 +0200

    Updated URL

---
+++ src/js/js-work/urlRewrite.js
@@ -0,0 +1,5 @@
+window.ec = window.ec || {};
+window.ec.config = window.ec.config || {};
+window.ec.config.storefrontUrls = window.ec.config.storefrontUrls || {};
+window.ec.config.storefrontUrls.cleanUrls = true;
+window.ec.config.storefrontUrls.queryBasedCleanUrls = true;
